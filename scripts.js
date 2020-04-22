a2d7839a980d65246fb2caddf0dac1b5ba13eee7
nctay <nctay@mail.ru>
2020-04-22 15:28:28 +0300

    rewrite

--- scripts.js
+++ scripts.js
@@ -1,45 +1,99 @@
-const form = $("form");
-const checklist = $(".checklist");
-const clear = $(".clear");
-const del = $(".del");
-const input = $("#item");
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
-localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+const form = document.getElementsByClassName('item-form').item(0)
+const checklist = document.getElementsByClassName('checklist').item(0)
+const clearAll = document.getElementsByClassName('clear-all').item(0)
+const clearDone =  document.getElementsByClassName('clear-done').item(0)
+const input = document.getElementsByTagName('input').item(0)
 
-const chMaker = text => {
-	const l = $("<label>", {class:"checkbox"});
-	const ch = $("<input>", {type: "checkbox"});
-	const s = $("<span>", {text:" " +text});
-	l.append(ch);
-	l.append(s);
-	checklist.append(l);
+// task getter
+const getTasks = () => {
+	const items = localStorage.getItem('tasks')
+	return items ? JSON.parse(items) : []
 }
 
-form.submit(function(e) {
-	e.preventDefault();
-	chMaker(input.val())
-	itemsArray.push(input.val());
-	localStorage.setItem('items', JSON.stringify(itemsArray));
-	input.val('')
-})
+// global task
+let userTasks = getTasks()
 
-data.forEach(item => {
-	chMaker(item);
-})
+// task setter
+const setTasksToStorage = () => {
+	localStorage.setItem('tasks', JSON.stringify(userTasks));
+}
 
-$(".clear").click(function(){
-	localStorage.clear();
-	$("label.checkbox").remove();
-	itemsArray = [];
-})
+// checkboxAdder
 
-$(".del").click(function() {
-	let prnt = $("input:checked").parent(".checkbox")
-	let hts = prnt.children('span').html()
-	let get = localStorage.getItem('items')
-	if (get == hts) {
-		localStorage.removeItem('items')
+const createCheckbox = (id, value, isChecked = false) => {
+	const container = document.createElement('div');
+	const checkbox = document.createElement('input')
+	const label = document.createElement('label');
+	checkbox.type = "checkbox";
+	checkbox.name = 'task';
+	checkbox.id = id
+	if(isChecked){
+		checkbox.checked = true
 	}
-	prnt.remove();
+	label.htmlFor = id;
+	label.appendChild(document.createTextNode(value));
+	container.appendChild(checkbox);
+	container.appendChild(label)
+	checklist.appendChild(container)
+	checkbox.addEventListener('change', setChecked)
+}
+
+// task renderer
+
+const renderTasks = tasks => {
+	tasks.forEach(({ id, value, isChecked }) => createCheckbox(id, value, isChecked))
+}
+
+// task appender
+const appendTask = task => {
+	const taskId = Math.random().toString(36).substr(2, 5)
+	createCheckbox(taskId, task)
+	const taskItem = {
+		id: taskId,
+		value: task,
+		isChecked: false
+	}
+	userTasks.push(taskItem)
+	setTasksToStorage()
+}
+
+// set checkbox checked
+const setChecked = e => {
+	const id = e.target.id
+	const isChecked = e.target.checked
+	userTasks.forEach(task => {
+		if(task.id === id){
+			task.isChecked = isChecked
+		}
+	})
+	setTasksToStorage()
+}
+// invoked functions
+renderTasks(userTasks)
+
+// submit
+form.addEventListener('submit', function (e) {
+	e.preventDefault()
+	appendTask(input.value)
+	input.value = ''
+})
+
+clearAll.addEventListener('click', ()=> {
+	localStorage.clear()
+	checklist.innerHTML = ''
+})
+
+clearDone.addEventListener('click', () => {
+	let newTasks = []
+	userTasks.forEach(task => {
+		const element = document.getElementById(task.id)
+		if(!task.isChecked) {
+			newTasks.push(task)
+			return
+		}
+		element.parentElement.remove()
+	})
+	userTasks = newTasks
+	console.log(userTasks)
+	setTasksToStorage()
 })
