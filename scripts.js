d173169494f53f300677c84e18d1076fed0b1735
psychowarrior12 <>
2021-04-21 13:40:42 +0000

    Create new app

--- scripts.js
+++ scripts.js
@@ -1,153 +1,54 @@
-const form = document.getElementsByClassName('item-form').item(0)
-const checklist = document.getElementsByClassName('checklist').item(0)
-const clearAll = document.getElementsByClassName('clear-all').item(0)
-const clearDone =  document.getElementsByClassName('clear-done').item(0)
-const input = document.getElementsByClassName('item-input').item(0)
-const taskTitle = document.getElementsByClassName('title').item(0)
-const titleForm = document.getElementsByClassName('title-form').item(0)
-const titleInput = document.getElementsByClassName('title-input').item(0)
+const form = document.querySelector('form');
+const expressionInput = document.querySelector('#expression');
+const answerInput = document.querySelector('#answer');
+const btnGroup = document.querySelector('.btn-group');
+const buttons = btnGroup.querySelectorAll('button');
 
-// task getter
-const getTasks = () => {
-	const items = localStorage.getItem('tasks')
-	return items ? JSON.parse(items) : []
-}
+const state = {
+  expression: '',
+  result: '',
+};
 
-// global task
-let userTasks = getTasks()
+const operands = ['+', '-', '%C3%97', '%C3%B7'];
 
-// title getter
-const getTitle = () => {
-	const items = localStorage.getItem('title')
-	return items ? JSON.parse(items) : []
-}
+const switcher = {
+  '+': '+',
+  '-': '-',
+  '%C3%97': '*',
+  '%C3%B7': '/',
+};
 
-//global title
-let userTitle = getTitle()
-
-// task setter
-const setTasksToStorage = () => {
-	localStorage.setItem('tasks', JSON.stringify(userTasks));
-}
-
-// title setter
-const setTitleToStorage = () => {
-	localStorage.setItem('title', JSON.stringify(userTitle));
-}
-
-// titleAdder
-const createTitle = (id, value) => {
-	taskTitle.appendChild(document.createTextNode(value));
-	taskTitle.id = id;
-}
-
-// checkboxAdder
-const createCheckbox = (id, value, isChecked = false) => {
-	const container = document.createElement('div');
-	const checkbox = document.createElement('input')
-	const label = document.createElement('label');
-	checkbox.type = "checkbox";
-	checkbox.name = 'task';
-	checkbox.id = id
-	if(isChecked){
-		checkbox.checked = true
-	}
-	label.htmlFor = id;
-	checkbox.className = "task"
-	label.appendChild(document.createTextNode(value+';'));
-	container.appendChild(checkbox);
-	container.appendChild(label)
-	checklist.appendChild(container)
-	checkbox.addEventListener('change', setChecked)
-}
-
-// title renderer
-
-const renderTitle = title => {
-	title.forEach(({id, value}) => createTitle(id, value))
-}
-
-// task renderer
-
-const renderTasks = tasks => {
-	tasks.forEach(({ id, value, isChecked }) => createCheckbox(id, value, isChecked))
-}
-
-// title appender
-const appendTitle = title => {
-	const titleId = Math.random().toString(36).substr(2, 5)
-	createTitle(titleId, title)
-	const titleItem = {
-		value: title,
-		id: titleId
-	}
-	userTitle.push(titleItem)
-	setTitleToStorage();
-}
-
-// task appender
-const appendTask = task => {
-	const taskId = Math.random().toString(36).substr(2, 5)
-	createCheckbox(taskId, task)
-	const taskItem = {
-		id: taskId,
-		value: task,
-		isChecked: false
-	}
-	userTasks.push(taskItem)
-	setTasksToStorage()
-}
-
-// set checkbox checked
-const setChecked = e => {
-	const id = e.target.id
-	const isChecked = e.target.checked
-	userTasks.forEach(task => {
-		if(task.id === id){
-			task.isChecked = isChecked
-		}
-	})
-	setTasksToStorage()
-}
-// invoked functions
-renderTasks(userTasks)
-renderTitle(userTitle)
-
-// submit
-form.addEventListener('submit', function (e) {
-	e.preventDefault()
-	appendTask(input.value)
-	input.value = ''
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const answer = formData.get('answer');
+  const validExpression = state.expression.split('').map((item) => {
+    const encoded = encodeURI(item);
+    if (operands.indexOf(encoded) !== -1) {
+      return switcher[encoded];
+    }
+    return item;
+  }).join('');
+  state.result = eval(validExpression);
+  if (state.result === Number(answer)) {
+    answerInput.classList.remove('is-invalid');
+    answerInput.classList.add('valid');
+  } else {
+    answerInput.classList.remove('valid');
+    answerInput.classList.add('is-invalid');
+  }
+  answerInput.focus();
+  console.log(state)
 })
+  
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    expressionInput.value += e.target.innerText;
+    expressionInput.focus();
+  });
+});
 
-// submitTitle
-titleForm.addEventListener('submit', function (e){
-	e.preventDefault()
-	userTitle = []
-	taskTitle.innerHTML = ''
-	appendTitle(titleInput.value)
-	titleInput.value = ''
-})
-
-clearAll.addEventListener('click', ()=> {
-	localStorage.clear()
-	userTitle = []
-	userTasks = []
-	checklist.innerHTML = ''
-	taskTitle.innerHTML = ''
-})
-
-clearDone.addEventListener('click', () => {
-	let newTasks = []
-	userTasks.forEach(task => {
-		const element = document.getElementById(task.id)
-		if(!task.isChecked) {
-			newTasks.push(task)
-			return
-		}
-		element.parentElement.remove()
-	})
-	userTasks = newTasks
-	console.log(userTasks)
-	setTasksToStorage()
+expressionInput.addEventListener('input', (e) => {
+  state.expression = e.target.value;
+  console.log(state)
 })
